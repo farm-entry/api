03d78ee1eede1eae8e948dd98a8c52e4e1341346
Matt Woelfel <mwoelfel13@gmail.com>
2025-11-26 17:19:38 -0600

    commented out misc router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import userRouter from "./UserRoutes.js";
 import livestockRouter from "./LivestockRoutes.js";
-import miscRouter from "./MiscRoutes.js";
+// import miscRouter from "./MiscRoutes.js";
 
 const router: Router = Router();
 router.use("/user", userRouter);
 router.use("/livestock", livestockRouter);
-router.use("", miscRouter);
+// router.use("", miscRouter);
 
 export default router;
